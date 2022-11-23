b5a08bb7f676bcb599331d678ba028c251c9530d
Mhezel Mohammad <mhezelkhan@gmail.com>
2022-11-23 10:25:49 +0800

    training day 3 activity on Handling List - 4th commit

---
+++ training_day3.py
@@ -0,0 +1,38 @@
+#####################-------------------HANDLING LIST - OPERATION-------------------------#############################
+#Set items are unordered, unchangeable, and do not allow duplicate values.
+
+#list = {1, 2, 3, 4, 5}
+#print(frozenset(list))
+
+#myset = {"apple","banna","cherry"}
+#print(myset)
+#print(len(myset))
+
+#set1 = {"apple", "banana", "cherry"}
+#set2 = {1, 5, 7, 9, 3}
+#set3 = {True, False, False}
+
+#thisset = {"apple", "banana", "cherry"}
+#tropical = {"pineapple", "mango", "papaya"}
+#mylist = ["kiwi", "orange"]
+
+#for x in thisset:
+  #print(x)
+
+#thisset.add("orange")
+#thisset.update(tropical)
+#print(thisset)
+#thisset.update(mylist)
+#print(thisset)
+
+#set3 = thisset.union(tropical)
+#print(set3)
+
+
+
+
+
+
+
+
+
