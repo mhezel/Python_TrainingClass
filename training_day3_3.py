b00adcde033d846452537a84e85c2aa11780939d
Mhezel Mohammad <mhezelkhan@gmail.com>
2022-11-23 15:07:01 +0800

    training day 3 activity on OOP-Python - 7th commit - create classes and instantiate 3 objects in a class

--- training_day3_3.py
+++ training_day3_3.py
@@ -3,12 +3,15 @@
 #BEHAVIOR - METHODS
 
 class Car:
+    counter=0
     def __init__(self, color, model, manufacturer):
         self.color = color
         self.model = model
         self.manufacturer = manufacturer
     def display_car(self):
          print(self.color, self.model, self.manufacturer)
+    def car_count(self):
+        print(Car.counter)
 
 car = Car("blue", "civic", "honda")
 car.display_car()
@@ -18,4 +21,17 @@ car2.display_car()
 
 car3 = Car("black", "montero", "mitsubishi")
 car3.display_car()
+
+class Dog:
+    def __greet(): #private
+        print('hello')
+    def _greet2(): #protected
+        print('hello2')
+    def greet3():  # public
+            print('hello3')
+
+puppy = Dog
+puppy.greet3()#-->public
+puppy._greet2() #-->protected
+puppy._greet()#-->unable to call outside the class-->private
 
